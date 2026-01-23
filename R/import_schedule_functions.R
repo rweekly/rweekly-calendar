11d1161a99a04c18a50997feb8a6c297892f6509
Eric Nantz <theRcast@gmail.com>
2026-01-22 23:28:05 -0500

    fix: use pins versions of schedule and curator data

--- R/import_schedule_functions.R
+++ R/import_schedule_functions.R
@@ -1,58 +1,25 @@
-import_schedule_data <- function(
-  query = "SELECT * FROM schedule",
-  from_owner = "rweekly-org",
-  from_repo_name = "curation-schedule"
-) {
-
-  result_raw <- httr2::request("https://www.dolthub.com") |>
-  httr2::req_url_path_append(
-    "api",
-    "v1alpha1",
-    from_owner,
-    from_repo_name
-  ) |>
-    httr2::req_url_query(
-      q = query
-    ) |>
-    httr2::req_perform()
-
-  result_json <- httr2::resp_body_json(result_raw)
-
-  result_df <- purrr::map_df(
-    purrr::pluck(result_json, "rows"),
-    ~{
-      data_row <- purrr::map_depth(.x, 1, ~ifelse(is.null(.x), NA, .x))
-      return(tibble::as_tibble_row(data_row))
-    }
-  ) |>
-    dplyr::mutate(
-      issue_index = as.numeric(issue_index),
-      from = as.Date(from),
-      to = as.Date(to)
+import_schedule_data <- function(board = NULL) {
+  if (is.null(board)) {
+    board <- pins::board_s3(
+      bucket = "rweekly-misc",
+      prefix = "calendar/",
+      access_key = Sys.getenv("S3_ACCESS_KEY"),
+      secret_access_key = Sys.getenv("S3_SECRET_KEY"),
+      endpoint = Sys.getenv("S3_ENDPOINT")
     )
-  return(result_df)
+  }
+  board |> pins::pin_read("schedule_df")
 }
 
-import_curator_data <- function(query = "SELECT * FROM curator_df") {
-
-  result_raw <- httr2::request("https://www.dolthub.com") |>
-    httr2::req_url_path_append(
-      "api",
-      "v1alpha1",
-      "rweekly-org",
-      "curation-schedule"
-    ) |>
-      httr2::req_url_query(
-        q = query
-      ) |>
-      httr2::req_perform()
-  
-  result_json <- httr2::resp_body_json(result_raw)
-
-  result_df <- purrr::map_df(
-    purrr::pluck(result_json, "rows"),
-    ~tibble::as_tibble_row(.x)
-  ) 
-  return(result_df)
-
+import_curator_data <- function(board = NULL) {
+  if (is.null(board)) {
+    board <- pins::board_s3(
+      bucket = "rweekly-misc",
+      prefix = "calendar/",
+      access_key = Sys.getenv("S3_ACCESS_KEY"),
+      secret_access_key = Sys.getenv("S3_SECRET_KEY"),
+      endpoint = Sys.getenv("S3_ENDPOINT")
+    )
+  }
+  board |> pins::pin_read("curator_df")
 }
